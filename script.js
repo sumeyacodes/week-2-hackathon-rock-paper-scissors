71069fb6d0e2e086b3667d30d096687f1669cc9d
sumeyacodes <sumeya4h@gmail.com>
2024-12-05 18:39:55 +0000

    psuedo code & beginning to write code

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-console.log("Test")
+// INITIALISE VARIABLES //
 
-let userInput = window.prompt('What is your search term?');
-console.log(userInput)
+    let rockPaperScissorsArr = ["Rock", "Paper", "Scissors"]
+    let computerChoice = ""
+    let score = 0
+    
+// WINDOW PROMPT TO GET USER INPUT  //
+    
+    let userChoice = window.prompt("Welcome to our game, please type either Rock, Paper or Scissors to begin!");
+    
+
+// DECLARE FUNCTION WITH PARAMETER - userinPut //
+
+    // Get random index for computer to select a choice from the array
+        let randomIndex = Math.floor(Math.random() * 3);
+        computerChoice = rockPaperScissorsArr[randomIndex];
+        console.log (computerChoice);
+
+    // Check combinations to determine if the user wins, loses, or draws based on user and computer choices
+        if (computerInput === "Rock" && userInput === "Paper") {
+            score++;
+            console.log("USER WINS");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Rock" && userInput === "Rock") {
+            console.log("DRAW");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Rock" && userInput === "Scissors") {
+            score--;
+            console.log("USER LOSES");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Paper" && userInput === "Paper") {
+            console.log("DRAW");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Paper" && userInput === "Rock") {
+            score--;
+            console.log("USER LOSES");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Paper" && userInput === "Scissors") {
+            score++;
+            console.log("USER WINS");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Scissors" && userInput === "Paper") {
+            score--;
+            console.log("USER LOSES");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Scissors" && userInput === "Rock") {
+            score++;
+            console.log("USER WINS");
+            console.log(`Score is ${score}`);
+        } else if (computerInput === "Scissors" && userInput === "Scissors") {
+            console.log("DRAW");
+            console.log(`Score is ${score}`);
+        }
+
+
